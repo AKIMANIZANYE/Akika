f1af4427add9116cd9fbe677b7963dacd7594552
Claudine <you@example.com>
2019-11-22 16:50:47 +0200

    yes

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/wecode/Desktop/Akika-BeeHive/virtual"
+set -gx VIRTUAL_ENV "/home/claudine/Documents/Akika-BeeHive/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
